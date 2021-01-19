2998b085df1e2fa739b940165b9563d1e2eb4693
oshin-bosako <dantalianschariot@gmail.com>
2021-01-19 09:26:23 +0000

    Initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,64 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import ExtLink from './ext-link'
+import { useRouter } from 'next/router'
+import styles from '../styles/header.module.css'
+import Analytics from './analytics'
+
+const navItems: { label: string; page?: string; link?: string }[] = [
+  { label: 'Home', page: '/' },
+  { label: 'Blog', page: '/blog' },
+  { label: 'About', page: '/blog/profile' },  
+]
+  {/*  { label: 'Contact', page: '/contact' },　
+  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' }, */}
+
+const ogImageUrl = 'https://bosako.dev/og-image.jpg'
+
+export default ({ titlePre = '' }) => {
+  const { pathname } = useRouter()
+
+  return (
+    <header className={styles.header}>
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-T3S64HDVW3`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(){ window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-T3S64HDVW3}');})`,
+          }}
+        />
+        <title>{titlePre ? `${titlePre} |` : ''} Bosako Lab</title>
+        <meta
+          name="description"
+          content="Try everything."
+        />
+        <meta name="og:title" content="Bosako Lab" />
+        <meta property="og:image" content={ogImageUrl} />
+        <meta name="twitter:site" content="@oshinbosako" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={ogImageUrl} />
+        <Analytics />
+      </Head>
+      <ul>
+        {navItems.map(({ label, page, link }) => (
+          <li key={label}>
+            {page ? (
+              <Link href={page}>
+                <a className={pathname === page ? 'active' : undefined}>
+                  {label}
+                </a>
+              </Link>
+            ) : (
+              <ExtLink href={link}>{label}</ExtLink>
+            )}
+          </li>
+        ))}
+      </ul>
+    </header>
+  )
+}
+
+
---
+++ src/lib/blog-helpers.ts
@@ -0,0 +1,30 @@
+export const getBlogLink = (slug: string) => {
+  return `/blog/${slug}`
+}
+
+export const getDateStr = date => {
+  return new Date(date).toLocaleString('ja-JP', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  })
+}
+
+export const postIsPublished = (post: any) => {
+  return post.Published === 'Yes'
+}
+
+export const normalizeSlug = slug => {
+  if (typeof slug !== 'string') return slug
+
+  let startingSlash = slug.startsWith('/')
+  let endingSlash = slug.endsWith('/')
+
+  if (startingSlash) {
+    slug = slug.substr(1)
+  }
+  if (endingSlash) {
+    slug = slug.substr(0, slug.length - 1)
+  }
+  return startingSlash || endingSlash ? normalizeSlug(slug) : slug
+}
