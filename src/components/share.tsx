91c2a9db25ad10ad513e452186601742ae03805f
oshin-bosako <dantalianschariot@gmail.com>
2021-01-21 13:00:21 +0900

    Change shared library

--- src/components/share.tsx
+++ src/components/share.tsx
@@ -16,28 +16,9 @@ type Props = {
 export default ({ text, url }: Props) => {
   return (
     <>
-      <ul>
-        <li>
-          <TwitterShareButton url={url} title={text}>
-            <TwitterIcon size={32} round={true} />
-          </TwitterShareButton>
-        </li>
-        <li>
-          <FacebookShareButton url={url}>
-            <FacebookIcon size={32} round={true} />
-          </FacebookShareButton>
-        </li>
-        <li>
-          <HatenaShareButton url={url}>
-            <HatenaIcon size={32} round={true} />
-          </HatenaShareButton>
-        </li>
-      </ul>
-      <style jsx>{`
-        ul {
-          list-style: none;
-        }
-      `}</style>
+      <TwitterShareButton url={url} title={text}>
+        test
+      </TwitterShareButton>
     </>
   )
 }
