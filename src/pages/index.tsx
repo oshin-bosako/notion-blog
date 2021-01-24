e4c84bd7533c633229a0fcb4145b32414b116f8b
oshin-bosako <dantalianschariot@gmail.com>
2021-01-24 18:13:21 +0900

    Add blog list  to top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,67 +4,83 @@ import ExtLink from '../components/ext-link'
 import Features from '../components/features'
 import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
+import blogStyles from '../styles/blog.module.css'
 
-export default () => (
-  <>
-    <Header titlePre="Home" />
-    <div className={sharedStyles.layout}>
-      <img
-        src="/pic.jpg"
-        height="85"
-        width="250"
-        alt="ferrules"
-      />
-      <h1>Bosako Lab</h1>
-      <h2>Try everything :)
-      {/*  
-        Blazing Fast Notion Blog with Next.js'{' '}
-        <ExtLink
-          href="https://github.com/vercel/next.js/issues/9524"
-          className="dotted"
-          style={{ color: 'inherit' }}
-        >
-          SSG
-        </ExtLink>
-      */}
-      </h2>
+import { getBlogLink, getDateStr, postIsPublished } from '../lib/blog-helpers'
+import { textBlock } from '../lib/notion/renderers'
+import getNotionUsers from '../lib/notion/getNotionUsers'
+import getBlogIndex from '../lib/notion/getBlogIndex'
 
-      {/*  　<Features />　*/}
+export async function getStaticProps({ preview }) {
+  const postsTable = await getBlogIndex()
 
-      <div className="explanation">
-        {/*   
-        <p>
-          This is a statically generated{' '}
-          <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-          <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>.
-          It leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
-        </p>
+  const authorsToGet: Set<string> = new Set()
+  const posts: any[] = Object.keys(postsTable)
+    .map(slug => {
+      const post = postsTable[slug]
+      // remove draft posts in production
+      if (!preview && !postIsPublished(post)) {
+        return null
+      }
+      post.Authors = post.Authors || []
+      for (const author of post.Authors) {
+        authorsToGet.add(author)
+      }
+      return post
+    })
+    .filter(Boolean)
 
-        <p>
-          Get started by creating a new page in Notion and clicking the deploy
-          button below. After you supply your token and the blog index id (the
-          page's id in Notion) we will automatically create the table for you!
-          See{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-            here in the readme
-          </ExtLink>{' '}
-          for finding the new page's id. To get your token from Notion, login
-          and look for a cookie under www.notion.so with the name `token_v2`.
-          After finding your token and your blog's page id you should be good to
-          go!
-        </p>  */}
+  const { users } = await getNotionUsers([...authorsToGet])
+
+  posts.map(post => {
+    post.Authors = post.Authors.map(id => users[id].full_name)
+  })
+
+  return {
+    props: {
+      preview: preview || false,
+      posts,
+    },
+    unstable_revalidate: 10,
+  }
+}
+
+export default ({ posts = [], preview }) => {
+  return (
+    <>
+      <Header titlePre="Home" />
+      <div className="home_title">
+        <img src="/pic.jpg" height="85" width="250" alt="ferrules" />
+        <h1>Bosako Lab</h1>
+        <h2>Try everything :)</h2>
       </div>
-    </div>
-  </>
-)
+
+      <div className={`${sharedStyles.layout} ${blogStyles.blogIndex}`}>
+        <h1>すべての記事</h1>
+        {posts.length === 0 && (
+          <p className={blogStyles.noPosts}>There are no posts yet</p>
+        )}
+        {posts.map(post => {
+          return (
+            <div className={blogStyles.postPreview} key={post.Slug}>
+              <h3>
+                <Link href="/blog/[slug]" as={getBlogLink(post.Slug)}>
+                  <div className={blogStyles.titleContainer}>
+                    {!post.Published && (
+                      <span className={blogStyles.draftBadge}>Draft</span>
+                    )}
+                    <a>{post.Page}</a>
+                  </div>
+                </Link>
+              </h3>
+              {post.Date && (
+                <span className="posted">🕒{getDateStr(post.Date)},</span>
+              )}
+              {post.Tag.length > 0 && <span className="tag">{post.Tag}</span>}
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
