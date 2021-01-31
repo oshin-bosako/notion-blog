3a40b37d03a8a943bea79e4355569206fb60c991
oshin-bosako <dantalianschariot@gmail.com>
2021-01-31 15:27:33 +0900

    Add google-analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,28 @@ import '../styles/global.css'
 import 'katex/dist/katex.css'
 import Footer from '../components/footer'
 import 'react-medium-image-zoom/dist/styles.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag.js'
 
-export default ({ Component, pageProps }) => (
-  <>
-    <Component {...pageProps} />
-    <Footer />
-  </>
-)
+export default ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      if (location.host === 'alpacat.com') {
+        gtag.pageview(url)
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
+}
