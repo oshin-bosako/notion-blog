90ed917c3c8a4c43385f67226c814954ff1e05f1
oshin-bosako <dantalianschariot@gmail.com>
2021-01-31 17:27:45 +0900

    Add footer text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,25 +1,18 @@
 import ExtLink from './ext-link'
+import styles from '../styles/footer.module.css'
 
 export default () => (
   <>
     <footer>
-      {/*
-      <span>Deploy your own!</span>
-      <ExtLink href="https://vercel.com/import/git?s=https://github.com/ijjk/notion-blog/tree/master&env=NOTION_TOKEN,BLOG_INDEX_ID&envDescription=Required+env+values+for+deploying&envLink=https://github.com/ijjk/notion-blog%23getting-blog-index-and-token">
-        <img
-          src="https://vercel.com/button"
-          height={46}
-          width={132}
-          alt="deploy to Vercel button"
-        />
-      </ExtLink>
-      <span>
-        or{' '}
-        <ExtLink href="https://github.com/ijjk/notion-blog">
-          view source
-        </ExtLink>
-      </span>
-      */}
+      <hr />
+      <div className="footer_text">
+        <p className="footer_logo">Bosako Lab.</p>
+        <p>このブログはNotion Blogをカスタマイズしています。</p>
+        <p>
+          オリジナルは
+          <a href="https://github.com/ijjk/notion-blog">ijjk/notion-blog</a>です
+        </p>
+      </div>
     </footer>
   </>
 )
