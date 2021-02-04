16b1dce78b9699e1a1d69fa6641d59248f2abda4
oshin-bosako <dantalianschariot@gmail.com>
2021-02-04 17:01:25 +0900

    Change image width 350 to 500

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -285,7 +285,7 @@ const RenderPost = ({ post, redirect, preview, baseBlogUrl }) => {
                     loop={!isImage}
                     muted={!isImage}
                     autoPlay={!isImage}
-                    style={{ width:350, marginBottom: 4 }}
+                    style={{ width:500, marginBottom: 4 }}
                     className={blogStyles.postImg}
                   />
                 </Zoom>
