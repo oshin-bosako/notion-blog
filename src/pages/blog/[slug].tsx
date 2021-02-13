310e83de87a9f5732b4f7a0fbece58b0cc9567e9
oshin-bosako <dantalianschariot@gmail.com>
2021-02-13 23:38:41 +0900

    Fix react-medium-image-zoom 2

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -291,7 +291,7 @@ const RenderPost = ({ post, redirect, preview, baseBlogUrl }) => {
                     loop={!isImage}
                     muted={!isImage}
                     autoPlay={!isImage}
-                    style={{ width:350, marginBottom: 4 }}
+                    style={{ width:'80%', marginBottom: 4 }}
                     className={blogStyles.postImg}
                   />
                 </Zoom>
